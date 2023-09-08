be49dc99d3d68e8a5b0384706edcfab04580d707
Juan Lowe <108947122+juannlowe@users.noreply.github.com>
2023-09-08 14:14:08 -0700

    added server "api"

--- vans.jsx
+++ vans.jsx
@@ -2,8 +2,22 @@ import React from "react"
 import { Link } from "react-router-dom"
 
 export default function Vans(){
-    
+    const [vans, setVans] = React.useState([]);
+    React.useEffect(()=>{
+        fetch("/api/vans")
+        .then (res=>res.json())
+        .then (data => setVans(data.vans))
+    }, [])
+const vanElements = vans.map(van => (
+    <div>
+        <h3>{van.name}</h3>
+    </div>
+))
+
     return (
+        <div>
         <h2>vans here to choose</h2>
+        {vanElements}
+        </div>
     )
 }
