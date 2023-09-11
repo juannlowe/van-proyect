d6f54ca3cd25dcb16df6e7dfa8ab8e465d0df165
Juan Lowe <108947122+juannlowe@users.noreply.github.com>
2023-09-11 14:01:02 -0700

    changes still some work firebase

--- api.js
+++ api.js
@@ -1,8 +1,15 @@
 
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore"
-import { initializeApp } from "firebase/app";
-
+import {
+    getFirestore,
+    collection,
+    doc,
+    getDocs,
+    getDoc,
+    query,
+    where,
+    documentId
+} from "firebase/firestore/lite"
 
 const firebaseConfig = {
   apiKey: "AIzaSyCZgO0lacP3Y3yLuGYJ2-cqqn3otW2Uup8",
@@ -15,41 +22,43 @@ const firebaseConfig = {
 
 
 const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
 
+// Refactoring the fetching functions below
+const vansCollectionRef = collection(db, "vans")
 
-
-
-
-
-
-
-export async function getVans(id) {
-    const url = id ? `/api/vans/${id}` : "/api/vans"
-    const res = await fetch(url)
-    if (!res.ok) {
-        throw {
-            message: "Failed to fetch vans",
-            statusText: res.statusText,
-            status: res.status
-        }
-    }
-    const data = await res.json()
-    return data.vans
+export async function getVans() {
+    const snapshot = await getDocs(vansCollectionRef)
+    const vans = snapshot.docs.map(doc => ({
+        ...doc.data(),
+        id: doc.id
+    }))
+    return(vans)
 }
 
-export async function getHostVans(id) {
-    const url = id ? `/api/host/vans/${id}` : "/api/host/vans"
-    const res = await fetch(url)
-    if (!res.ok) {
-        throw {
-            message: "Failed to fetch vans",
-            statusText: res.statusText,
-            status: res.status
-        }
+export async function getVan(id) {
+    const docRef = doc(db, "vans", id)
+    const snapshot = await getDoc(docRef)
+    return {
+        ...snapshot.data(),
+        id: snapshot.id
     }
-    const data = await res.json()
-    return data.vans
 }
+
+export async function getHostVans() {
+    const q = query(vansCollectionRef, where("hostId", "==", "123"))
+    const snapshot = await getDocs(q)
+    const vans = snapshot.docs.map(doc => ({
+        ...doc.data(),
+        id: doc.id
+    }))
+    return vans
+}
+
+/* 
+This 👇 isn't normally something you'd need to do. Instead, you'd 
+set up Firebase security rules so only the currently logged-in user 
+could edit their vans.
 
 export async function loginUser(creds) {
     const res = await fetch("/api/login",
@@ -66,4 +75,4 @@ export async function loginUser(creds) {
     }
 
     return data
-}
+}*/
