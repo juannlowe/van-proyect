b95a16b65f272cf441dfd8d7c03e7efc727e21e4
Juan Lowe <108947122+juannlowe@users.noreply.github.com>
2023-09-18 18:13:10 -0700

    button updates- error

--- components/Error.jsx
+++ components/Error.jsx
@@ -2,6 +2,9 @@ import React from "react"
 
 export default function Error(){
     return (
+        <>
         <h1>An Error Ocurred!</h1>
+        <button>Click here to return to home</button>
+        </>
     )
 }
